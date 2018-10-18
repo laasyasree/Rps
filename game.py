bf63221f91ab407deecc6f00109b12880c9c7752
athiranair2000 <laasyasreesapa@gmail.com>
2018-10-18 16:54:33 +0530

    rock paper scissiors

---
+++ game.py
@@ -0,0 +1,40 @@
+import random
+print("Let's start the game..enter an input")
+list=["rock","paper","scissors"]
+c=0
+d=0
+for i in range(0,5):
+    a=input()
+    b=random.choice(list)
+    while(a!="rock" and a!="paper" and a!="scissors"):
+        print("enter either rock,paper or scissors")
+        a=input()
+    if(a=="rock"):
+        if(b=="scissors"):
+            print("you won")
+            c=c+1
+        else:
+            print("you lost")
+            d=d+1
+    elif(a=="paper"):
+        if(b=="rock"):
+            print("you won")
+            c=c+1
+        else:
+            print("you lost")
+            d=d+1
+    elif(a=="scissors"):
+        if(b=="paper"):
+            print("you won")
+            c=c+1
+        else:
+            print("you lost")
+            d=d+1
+    elif(a==b):
+        print("tie")
+if(c>d):
+    print("you r the winner")
+elif(c<d):
+    print("you r the looser")
+else:
+    print("both r equal")
